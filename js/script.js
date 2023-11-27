0992f84523c97d9e93cd0b5b75be4f272ff899c8
Iryna <mefodievna2908@gmail.com>
2023-11-27 21:32:26 +0200

    some work

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const elemsOne = document.querySelectorAll('.lesson_list');
+console.log(elemsOne);
+
+const elemsTwo  = document.querySelectorAll('li');
+console.log(elemsTwo);
+
+const elemsThree = document.querySelectorAll('li.lesson__item-list');
+console.log(elemsThree);
+
+const elemsFour = document.querySelectorAll('.lesson__list>li');
+console.log(elemsFour);
+
+const elemsFive = document.querySelectorAll('.lesson__list, .lesson__text');
+console.log(elemsFive);
+
+const elemsSix = document.querySelectorAll('.lesson__list .lesson__text');
+console.log(elemsSix);
+
+const elemsSeven = document.querySelectorAll('#listItem');
+console.log(elemsSeven);
+
+const elemsEight = document.querySelectorAll('[data-item]');
+console.log(elemsEight);
